6f0472a0393ccb0f4ade92c0746061c4b3772f09
root <root@server.casa>
2024-02-12 17:05:44 +0100

    Added disclaimers

---
+++ README.md
@@ -0,0 +1,8 @@
+
+By Willy Gardiol, provided under the GPLv3 License. https://www.gnu.org/licenses/gpl-3.0.html
+Publicly available at: https://github.com/gardiol/routes-setup
+You can contact me at willy@gardiol.org
+
+====
+
+
--- routes-setup.sh
+++ routes-setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# By Willy Gardiol, provided under the GPLv3 License. https://www.gnu.org/licenses/gpl-3.0.html
+# Publicly available at: https://github.com/gardiol/routes-setup
+# You can contact me at willy@gardiol.org
 
 export myself=$0
 
